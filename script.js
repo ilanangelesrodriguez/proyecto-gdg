f27df0c2b7ba858aa882e3733291f57254a2f15f
ilanangelesrodriguez <ilanangelesrodriguez@gmail.com>
2024-06-22 16:29:22 +0000

    feat: añadir mejoras

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
-console.log("¡Hola desde JavaScript!");
 
-function cambiarTexto() {
-    document.getElementById("miParrafo").innerHTML = "¡El texto ha cambiado!";
+let contadorCampos = 0;
+
+function agregarCampo() {
+    contadorCampos++;
+
+    let nuevoCampo = document.createElement('div');
+    nuevoCampo.classList.add('campo');
+    nuevoCampo.innerHTML = `
+        <label for="campo${contadorCampos}">Campo ${contadorCampos}:</label><br>
+        <input type="text" id="campo${contadorCampos}" name="campo${contadorCampos}"><br><br>
+    `;
+
+    document.getElementById('camposDinamicos').appendChild(nuevoCampo);
 }
+
+document
+.getElementById('miFormulario').addEventListener('submit', function(event) {
+    event.preventDefault(); // Evita que el formulario se envíe de forma tradicional
+});
